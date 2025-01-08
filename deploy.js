a2a016d1d4b88912c46ed06bee5aef1862090d78
leighton075 <leighton.simmons075@gmail.com>
2025-01-09 12:56:32 +1300

    updated deploy

--- deploy.js
+++ deploy.js
@@ -8,15 +8,19 @@ const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
-    const commandsPath = path.join(foldersPath, folder);
-    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-    for (const file of commandFiles) {
-        const filePath = path.join(commandsPath, file);
-        const command = require(filePath);
-        if ('data' in command && 'execute' in command) {
-            commands.push(command.data.toJSON());
-        } else {
-            console.error(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+    const folderPath = path.join(foldersPath, folder);
+
+    if (fs.statSync(folderPath).isDirectory()) {
+        const commandFiles = fs.readdirSync(folderPath).filter(file => file.endsWith('.js'));
+
+        for (const file of commandFiles) {
+            const filePath = path.join(folderPath, file);
+            const command = require(filePath);
+            if ('data' in command && 'execute' in command) {
+                commands.push(command.data.toJSON());
+            } else {
+                console.error(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+            }
         }
     }
 }
