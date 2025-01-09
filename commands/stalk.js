66dfcf583446a7299aa8f2cbbd8c7aa7e139b0d1
leighton075 <leighton.simmons075@gmail.com>
2025-01-09 13:44:19 +1300

    add 404 filtering to stalk command

--- commands/stalk.js
+++ commands/stalk.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require('discord.js');
 const { ApifyClient } = require('apify-client');
+const fetch = require('node-fetch');
 
 const client = new ApifyClient({ token: process.env.APIFY_API_KEY });
 
@@ -7,14 +8,29 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('stalk')
         .setDescription('Find all instances of a username')
-        .addStringOption(option =>
-            option.setName('username')
-                .setDescription('Username to search for')
-                .setRequired(true)),
-
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('normal')
+                .setDescription('Just a regular search')
+                .addStringOption(option =>
+                    option.setName('username')
+                        .setDescription('Username to search for')
+                        .setRequired(true)))
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('advanced')
+                .setDescription('Search and filter out 404 links')
+                .addStringOption(option =>
+                    option.setName('username')
+                        .setDescription('Username to search for')
+                        .setRequired(true))),
+        
     async execute(interaction) {
         const username = interaction.options.getString('username');
         const startTime = Date.now();
+        const results = [];
+
+        const subcommand = interaction.options.getSubcommand();
 
         try {
             await interaction.reply({ content: `Searching for username: **${username}**...` });
@@ -23,30 +39,53 @@ module.exports = {
             const run = await client.actor('netmilk/sherlock').call(runInput);
 
             const dataset = client.dataset(run.defaultDatasetId);
-            const results = [];
 
-            // Fetch items from the dataset
             const { items } = await dataset.listItems();
+
             for (const item of items) {
                 if (item.links && item.links.length > 0) {
-                    results.push(...item.links);
+                    if (subcommand === 'advanced') {
+                        for (const link of item.links) {
+                            const isValidLink = await checkLink(link);
+                            if (isValidLink) {
+                                results.push(link);
+                            }
+                        }
+                    } else {
+                        results.push(...item.links);
+                    }
                 }
             }
 
             let response;
             if (results.length > 0) {
                 const foundLinks = results.map(link => `- ${link}`).join('\n');
-                response = `Found the username **"${username}"** on the following sites:\n${foundLinks}\nTime taken: ${Date.now() - startTime}ms`;
+                response = `Found the username **"${username}"** on the following sites:\n${foundLinks}\nTime taken: ${(Date.now() - startTime) / 1000}s`;
             } else {
-                response = `No results found for username **"${username}"**.\n\nTime taken: ${Date.now() - startTime}ms`;
+                response = `No valid results found for username **"${username}"**.\n\nTime taken: ${(Date.now() - startTime) / 1000}s`;
             }
 
-            await interaction.followUp(response);
+            await interaction.editReply(response);
 
         } catch (error) {
             console.error('Error executing /stalk command:', error);
             await interaction.followUp('An error occurred while searching. Please try again later.');
         }
-        console.log(`Execution time: ${(Date.now() - startTime) / 1000}ms`);
+        console.log(`Execution time: ${(Date.now() - startTime) / 1000}s`);
     },
 };
+
+// Helper function to check if the link is valid
+async function checkLink(link) {
+    try {
+        const response = await fetch(link, { method: 'HEAD' });
+        if (response.status === 404) {
+            console.log(`Link ${link} returned a 404 error.`);
+            return false;
+        }
+        return true;
+    } catch (error) {
+        console.error(`Error checking link ${link}:`, error);
+        return false;
+    }
+}
