c4ac1ce6c203971dbf9d3de5389ab190f79ceca4
leighton075 <leighton.simmons075@gmail.com>
2025-01-10 13:17:52 +1300

    follow up

--- commands/myip.js
+++ commands/myip.js
@@ -1,9 +1,10 @@
 const { SlashCommandBuilder } = require('discord.js');
+const axios = require('axios');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('getos')
-        .setDescription('Get a user\'s os based on their IP')
+        .setDescription('Get a user\'s OS based on their IP')
         .addStringOption(option =>
             option
                 .setName('ip')
@@ -13,6 +14,11 @@ module.exports = {
     async execute(interaction) {
         try {
             const ip = interaction.options.getString('ip');
+
+            await interaction.reply({
+                content: 'Fetching OS information... Please wait.',
+                ephemeral: true
+            });
 
             try {
                 const ipInfo = await axios.get(`https://ipinfo.io/${ip}/json`);
@@ -27,7 +33,7 @@ module.exports = {
                 await interaction.followUp(`Sorry, there was an error retrieving the OS information: ${error.message}`);
             }
         } catch (error) {
-            interaction.reply(`Sorry, there was an error retrieving your IP address: ${error.message}`);
+            await interaction.reply(`Sorry, there was an error: ${error.message}`);
         }
     },
 };
