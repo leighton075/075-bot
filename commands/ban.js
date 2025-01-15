874c69cc038578fdf7ae263984ca7cd6b75b78b2
Leighton075 <leighton.simmons075@gmail.com>
2025-01-15 23:53:04 +1300

    test

--- commands/ban.js
+++ commands/ban.js
@@ -35,74 +35,91 @@ module.exports = {
                 .setRequired(false)),
 
     async execute(interaction) {
-        if (!interaction.member.permissions.has(PermissionFlagsBits.KickMembers)) {
-            return interaction.reply({ content: "You don't have the permission to kick members." });
-        }
-
-        if (!interaction.guild.me.permissions.has(PermissionFlagsBits.KickMembers)) {
-            return interaction.reply({ content: "I don't have permission to kick members." });
-        }
-        
-        const user = interaction.options.getUser('user');
-        const reason = interaction.options.getString('reason') || 'No reason provided';
-
-        const confirm = new ButtonBuilder()
-            .setCustomId('confirm')
-            .setLabel('Confirm Ban')
-            .setStyle(ButtonStyle.Danger);
-
-        const cancel = new ButtonBuilder()
-            .setCustomId('cancel')
-            .setLabel('Cancel')
-            .setStyle(ButtonStyle.Secondary);
-
-        const row = new ActionRowBuilder()
-            .addComponents(cancel, confirm);
-
         try {
-            if (interaction.user.id === '1087801524282982450') {
-                return interaction.reply({ content: 'Good try oliver, tell me to fix this if you see this message' });
-            }
+            const userId = interaction.user.id;
 
-            await interaction.reply({
-                content: `Are you sure you want to ban ${user.tag} for reason: ${reason}?`,
-                components: [row],
-            });
+            const checkQuery = 'SELECT * FROM verification WHERE user_id = ?';
+            db.query(checkQuery, [userId], async (err, result) => {
+                if (err) {
+                    console.error(`[ERROR] Error checking user in the database: ${err}`);
+                    return interaction.reply('There was an error processing your request.');
+                }
 
-            const filter = i => i.user.id === interaction.user.id;
-            const collector = interaction.channel.createMessageComponentCollector({
-                filter,
-                time: 10000,
-            });
+                if (result.length === 0) {
+                    return interaction.reply('You need to verify your account first. Please verify your account using `/verify`.');
+                }
 
-            collector.on('collect', async i => {
-                if (i.customId === 'confirm') {
-                    try {
-                        await interaction.guild.members.ban(user, { reason });
+                if (!interaction.member.permissions.has(PermissionFlagsBits.BanMembers)) {
+                    return interaction.reply({ content: "You don't have the permission to ban members." });
+                }
 
-                        const embed = new EmbedBuilder()
-                            .setColor('#ff0000')
-                            .setTitle(`${user.tag} has been banned.`)
-                            .setDescription(`Banned by ${interaction.user.username}`)
-                            .setFooter({ text: `Reason for ban: ${reason}`, iconURL: interaction.user.displayAvatarURL() });
+                if (!interaction.guild.me.permissions.has(PermissionFlagsBits.BanMembers)) {
+                    return interaction.reply({ content: "I don't have permission to ban members." });
+                }
 
-                        await i.update({embeds: [embed], components: []});
-                        collector.stop();
-                    } catch (error) {
-                        console.error('Error banning user:', error);
-                        await i.update({ content: `There was an error banning ${user.tag}`, components: [] });
+                const user = interaction.options.getUser('user');
+                const reason = interaction.options.getString('reason') || 'No reason provided';
+
+                const confirm = new ButtonBuilder()
+                    .setCustomId('confirm')
+                    .setLabel('Confirm Ban')
+                    .setStyle(ButtonStyle.Danger);
+
+                const cancel = new ButtonBuilder()
+                    .setCustomId('cancel')
+                    .setLabel('Cancel')
+                    .setStyle(ButtonStyle.Secondary);
+
+                const row = new ActionRowBuilder().addComponents(cancel, confirm);
+
+                try {
+                    if (interaction.user.id === '1087801524282982450') {
+                        return interaction.reply({ content: 'Good try oliver, tell me to fix this if you see this message' });
                     }
-                } else if (i.customId === 'cancel') {
-                    await i.update({ content: 'Ban action has been canceled.', components: [] });
+
+                    await interaction.reply({
+                        content: `Are you sure you want to ban ${user.tag} for reason: ${reason}?`,
+                        components: [row],
+                    });
+
+                    const filter = i => i.user.id === interaction.user.id;
+                    const collector = interaction.channel.createMessageComponentCollector({
+                        filter,
+                        time: 10000,
+                    });
+
+                    collector.on('collect', async i => {
+                        if (i.customId === 'confirm') {
+                            try {
+                                await interaction.guild.members.ban(user, { reason });
+
+                                const embed = new EmbedBuilder()
+                                    .setColor('#ff0000')
+                                    .setTitle(`${user.tag} has been banned.`)
+                                    .setDescription(`Banned by ${interaction.user.username}`)
+                                    .setFooter({ text: `Reason for ban: ${reason}`, iconURL: interaction.user.displayAvatarURL() });
+
+                                await i.update({ embeds: [embed], components: [] });
+                                collector.stop();
+                            } catch (error) {
+                                console.error('Error banning user:', error);
+                                await i.update({ content: `There was an error banning ${user.tag}`, components: [] });
+                            }
+                        } else if (i.customId === 'cancel') {
+                            await i.update({ content: 'Ban action has been canceled.', components: [] });
+                        }
+                    });
+
+                    collector.on('end', (collected, reason) => {
+                        if (reason === 'time') {
+                            interaction.editReply({ content: 'You took too long to respond. Ban action canceled.', components: [] });
+                        }
+                    });
+                } catch (error) {
+                    console.error('Error during the ban confirmation process:', error);
+                    return interaction.reply({ content: `There was an error with the ban process.`, ephemeral: true });
                 }
             });
-
-            collector.on('end', (collected, reason) => {
-                if (reason === 'time') {
-                    interaction.editReply({ content: 'You took too long to respond. Ban action canceled.', components: [] });
-                }
-            });
-
         } catch (error) {
             console.error('Error during ban process:', error);
             return interaction.reply({ content: `There was an error with the ban process.`, ephemeral: true });
