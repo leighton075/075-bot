09145f37cec241d3b2ff081e1553e74e13b6ef09
Leighton075 <leighton.simmons075@gmail.com>
2025-01-25 18:20:59 +1300

    testing

--- commands/21.js
+++ commands/21.js
@@ -38,6 +38,13 @@ module.exports = {
 
                 if (result.length > 0) {
                     //testing again, again, again
+                    //
+                    ///
+                    ///
+                    ///
+                    ///
+                    ///
+                    ///
                     if (interaction.user.id === '1087801524282982450') {
                         return interaction.reply('No oliver');
                     }
